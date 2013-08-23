5c32009533b2ea51e1317f8aa915150539546225
Jxck <block.rxckin.beats@gmail.com>
2013-08-24 04:44:32 +0900

    add DecodeInteger

--- src/github.com/jxck/hpac/Encoder.go
+++ src/github.com/jxck/hpac/Encoder.go
@@ -42,10 +42,13 @@ func EncodeInteger(I int, N int) *bytes.Buffer {
 			log.Println("binary.Write failed:", err)
 		}
 
+		// I = I - (2^N - 1)
+		I = I - boundary
+
 		// While I >= 128
 		for I >= 128 {
 			// Encode (I % 128 + 128) on 8 bits
-			err := binary.Write(buf, binary.BigEndian, I%128+128)
+			err := binary.Write(buf, binary.BigEndian, uint8(I%128+128))
 			if err != nil {
 				log.Println("binary.Write failed:", err)
 			}
@@ -54,10 +57,25 @@ func EncodeInteger(I int, N int) *bytes.Buffer {
 		}
 
 		// encode (I) on 8 bits
-		err = binary.Write(buf, binary.BigEndian, I)
+		err = binary.Write(buf, binary.BigEndian, uint8(I))
 		if err != nil {
 			log.Println("binary.Write failed:", err)
 		}
 	}
+	log.Println(buf.Bytes())
 	return buf
+}
+
+func DecodeInteger(buf []byte, N uint8) uint32 {
+	boundary := byte(math.Pow(2, float64(N)) - 1)
+	if buf[0] == boundary {
+		l := len(buf) - 1
+		I := uint32(buf[l]) * 128
+		for l--; l > 1; l-- {
+			I = (I + uint32(buf[l]) - 128) * 128
+		}
+		I = I + uint32(buf[1]) - 128 + uint32(buf[0])
+		return uint32(I)
+	}
+	return uint32(buf[0])
 }
--- src/github.com/jxck/hpac/Encoder_test.go
+++ src/github.com/jxck/hpac/Encoder_test.go
@@ -21,3 +21,24 @@ func TestEncodeInteger(t *testing.T) {
 		}
 	}
 }
+
+func TestDecodeInteger(t *testing.T) {
+	buf := EncodeInteger(10, 5)
+	n := DecodeInteger(buf.Bytes(), 5)
+	if n != 10 {
+		t.Errorf("got %v\nwant %v", n, 10)
+	}
+
+	buf = EncodeInteger(1337, 5)
+	n = DecodeInteger(buf.Bytes(), 5)
+	if n != 1337 {
+		t.Errorf("got %v\nwant %v", n, 1337)
+	}
+
+	buf = EncodeInteger(3000000, 5) // [31 161 141 183 1]
+	n = DecodeInteger(buf.Bytes(), 5)
+	//t.Log((((1*128)+(183-128))*128+(141-128))*128+161-128+ 31)
+	if n != 3000000 {
+		t.Errorf("got %v\nwant %v", n, 3000000)
+	}
+}
