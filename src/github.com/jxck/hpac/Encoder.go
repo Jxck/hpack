7363e4064fd07cf38cfa6903626ac0d9c1cdcab7
Jxck <block.rxckin.beats@gmail.com>
2013-08-24 03:09:56 +0900

    update EncodeInteger with Draft-02

--- src/github.com/jxck/hpac/Encoder.go
+++ src/github.com/jxck/hpac/Encoder.go
@@ -23,6 +23,7 @@ func NewEncoder() Encoder {
 }
 
 func EncodeInteger(I int, N int) *bytes.Buffer {
+	log.Printf("%v %v = ", I, N)
 	buf := new(bytes.Buffer)
 
 	// 2^N -1
@@ -35,36 +36,28 @@ func EncodeInteger(I int, N int) *bytes.Buffer {
 			log.Println("binary.Write failed:", err)
 		}
 	} else {
-		// Else, encode 2^N - 1 on N bits and do the following steps:
+		// encode 2^N - 1 on N bits
 		err := binary.Write(buf, binary.BigEndian, uint8(boundary))
 		if err != nil {
 			log.Println("binary.Write failed:", err)
 		}
 
-		// Set I to (I - (2^N - 1)) and Q to 1
-		I = I - boundary
-		Q := 1
-		R := 0
-
-		for Q > 0 {
-			// Compute Q and R, quotient and remainder of I divided by 2^7
-			R = I % 128
-			Q = (I - R) / 128
-
-			// If Q is strictly greater than 0, write one 1 bit; otherwise, write one 0 bit
-			var b uint8 = 0
-			if Q > 0 {
-				b = 128
-			}
-
-			b = b | uint8(R)
-			err := binary.Write(buf, binary.BigEndian, b)
+		// While I >= 128
+		for I >= 128 {
+			// Encode (I % 128 + 128) on 8 bits
+			err := binary.Write(buf, binary.BigEndian, I%128+128)
 			if err != nil {
 				log.Println("binary.Write failed:", err)
 			}
-			I = Q
+			// I = I / 128
+			I = I / 128
+		}
+
+		// encode (I) on 8 bits
+		err = binary.Write(buf, binary.BigEndian, I)
+		if err != nil {
+			log.Println("binary.Write failed:", err)
 		}
 	}
-
 	return buf
 }
