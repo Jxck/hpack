0840f459e6064479155478dbc77b898c0e47da36
Jxck <block.rxckin.beats@gmail.com>
2013-08-27 22:43:44 +0900

    tmp

--- src/github.com/jxck/main.go
+++ src/github.com/jxck/main.go
@@ -1,17 +1,45 @@
 package main
 
 import (
-	. "github.com/jxck/hpac"
 	"log"
-	"net/http"
 )
 
-var header = http.Header{
-	":path":       []string{"/my-example/index.html"},
-	"user-agent":  []string{"my-user-agent"},
-	"mynewheader": []string{"first"},
+type HeaderName string
+type HeaderValue string
+
+type Header struct {
+	Name  HeaderName
+	Value HeaderValue
 }
 
+type HeaderTable []*Header // Header はポインタにしておく
+
+//func (ht *HeaderTable) Add(header Header) {
+//	*ht = append(*ht, header)
+//}
+
+func (ht HeaderTable) SearchHeader(name HeaderName) int {
+	for i, h := range ht {
+		if h.Name == name {
+			return i
+		}
+	}
+	return -1
+}
+
+func NewRequestHeaderTable() HeaderTable {
+	return HeaderTable{
+		{":scheme", "http"},
+		{":scheme", "https"},
+		{":host", "A"},
+		{":path", "/"},
+	}
+}
 func main() {
 	log.SetFlags(log.Lshortfile)
+
+	header := NewRequestHeaderTable()
+
+	i := header.SearchHeader(":host")
+	log.Println(header[i])
 }
