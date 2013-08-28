3fc6b9bc2f220871f518ebf2f1e29f33c2124320
Jxck <block.rxckin.beats@gmail.com>
2013-08-29 00:52:59 +0900

    add SearcHeader

--- src/github.com/jxck/main.go
+++ src/github.com/jxck/main.go
@@ -7,20 +7,60 @@ import (
 	"net/http"
 )
 
-func init() {
-	log.SetFlags(log.Lshortfile)
+type Header struct {
+	Name  string
+	Value string
 }
 
+// ヘッダはポインタにしておく
+type HeaderTable []*Header
+
+//func (ht *HeaderTable) Add(header Header) {
+//	*ht = append(*ht, header)
+//}
+
+func (ht HeaderTable) SearchHeader(name, value string) (int, *Header) {
+	var matching_name_indexes = []int{}
+	for i, h := range ht {
+		if h.Name == name {
+			if h.Value == value {
+				log.Println("index header")
+				return i, h
+			}
+			matching_name_indexes = append(matching_name_indexes, i)
+		}
+	}
+	if len(matching_name_indexes) > 0 {
+		log.Println("literal with index")
+		return matching_name_indexes[0], nil
+	}
+	log.Println("literal without index")
+	return -1, nil
+}
+
+func NewRequestHeaderTable() HeaderTable {
+	return HeaderTable{
+		{":scheme", "http"},
+		{":scheme", "https"},
+		{":host", ""},
+		{":path", "/"},
+	}
+}
 
 func main() {
-	var header = http.Header{
+	log.SetFlags(log.Lshortfile)
+
+	var headers = http.Header{
 		":scheme":     []string{"http"},
 		":path":       []string{"/index.html"},
 		"mynewheader": []string{"first"},
 	}
-	_ = header
 
-	//	TestIndexedHeader()
-	TestNewNameWithoutIndexing()
-	//TestIndexedNameWithoutIndexing()
+	headerTable := NewRequestHeaderTable()
+	for name, values := range headers {
+		for i := range values {
+			value := values[i]
+			log.Println(headerTable.SearchHeader(name, value))
+		}
+	}
 }
