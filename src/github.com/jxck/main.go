4b24c2daf4e01e9f69c6ea6d0edaba88d4b01268
Jxck <block.rxckin.beats@gmail.com>
2013-08-30 00:50:09 +0900

    add comments

--- src/github.com/jxck/main.go
+++ src/github.com/jxck/main.go
@@ -19,23 +19,42 @@ type HeaderTable []*Header
 //	*ht = append(*ht, header)
 //}
 
+// name と value が HeaderTable にあるかを探す
+// name, value とも一致 => index, *Header
+// name はある          => index, nil
+// ない                 => -1, nil
 func (ht HeaderTable) SearchHeader(name, value string) (int, *Header) {
+	// name が複数一致した時のために格納しておく
+	// MEMO: スライスで持たず単一で最初だけもってもいいかもしれないが
+	// もし無かった場合 0 になって、それが index=0 と紛らわしいので
+	// slice でもって、長さで判断できるようにした
 	var matching_name_indexes = []int{}
+
+	// ヘッダテーブルの頭から探す
 	for i, h := range ht {
+
+		// Name がヘッダテーブルにあった場合
 		if h.Name == name {
+
+			// Value も一致したら
 			if h.Value == value {
-				log.Println("index header")
-				return i, h
+				// 一致した index とそこにある値を返す
+				return i, h // index header
 			}
+
+			// name は一致したのでそのインデックスを加えておく
 			matching_name_indexes = append(matching_name_indexes, i)
 		}
 	}
+
+	// Name があっても value までは一致しなかった場合
+	// 一番最初のヘッダを返す
 	if len(matching_name_indexes) > 0 {
-		log.Println("literal with index")
-		return matching_name_indexes[0], nil
+		return matching_name_indexes[0], nil // literal with index
 	}
-	log.Println("literal without index")
-	return -1, nil
+
+	// Name も一致しなかったら -1, nil
+	return -1, nil // literal without index
 }
 
 func NewRequestHeaderTable() HeaderTable {
@@ -60,7 +79,14 @@ func main() {
 	for name, values := range headers {
 		for i := range values {
 			value := values[i]
-			log.Println(headerTable.SearchHeader(name, value))
+			index, h := headerTable.SearchHeader(name, value)
+			if h != nil {
+				log.Println("index header")
+			} else if index != -1 {
+				log.Println("literal with index")
+			} else {
+				log.Println("literal without index")
+			}
 		}
 	}
 }
