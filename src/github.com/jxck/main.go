805f424f3595d2d29e8e9e53ca27352748577150
Jxck <block.rxckin.beats@gmail.com>
2013-08-30 22:39:45 +0900

    adding encoder sample in main.go

--- src/github.com/jxck/main.go
+++ src/github.com/jxck/main.go
@@ -1,56 +1,69 @@
 package main
 
 import (
+	"bytes"
+	. "github.com/jxck/hpac"
 	"log"
 	"net/http"
 )
 
-type Header struct {
-	Name  string
-	Value string
+func init() {
+	log.SetFlags(log.Lshortfile)
 }
 
-// ヘッダはポインタにしておく
-type HeaderTable []*Header
+func EncodeHeader(frame Frame) *bytes.Buffer {
+	switch frame.(type) {
+	case *IndexedHeader:
+		f := frame.(*IndexedHeader)
+		return encodeIndexedHeader(f)
+//	case *NewNameWithoutIndexing:
+//		f := frame.(*NewNameWithoutIndexing)
+//		return encodeNewNameWithoutIndexing(f)
+	default:
+		log.Println("unmatch")
+		return nil
+	}
+}
 
-//func (ht *HeaderTable) Add(header Header) {
-//	*ht = append(*ht, header)
+func encodeIndexedHeader(frame *IndexedHeader) *bytes.Buffer {
+	index := frame.Index
+	buf := bytes.NewBuffer([]byte{index + 0x80})
+	return buf
+}
+
+//func encodeNewNameWithoutIndexing(frame *NewNameWithoutIndexing) *bytes.Buffer {
+//	buf := bytes.NewBuffer([]byte{0x60})
+//	buf.WriteByte(EncodeInteger(frame.NameLength, 8))
+//	buf.WriteString(frame.NameString)
+//	buf.WriteByte(EncodeInteger(frame.ValueLength, 8))
+//	buf.WriteString(frame.ValueString)
+//	return buf
 //}
 
-func (ht HeaderTable) SearchHeader(name, value string) (int, *Header) {
-	for i, h := range ht {
-		if h.Name == name {
-			if h.Value == value {
-				return i, h
-			}
-		}
-	}
-	return -1, nil
+func TestIndexedHeader() {
+	frame := NewIndexedHeader()
+	frame.Index = 10
+	buf := EncodeHeader(frame)
+	log.Printf("%v", buf.Bytes())
 }
 
-func NewRequestHeaderTable() HeaderTable {
-	return HeaderTable{
-		{":scheme", "http"},
-		{":scheme", "https"},
-		{":host", ""},
-		{":path", "/"},
-	}
-}
+//func NewNameWithoutIndexing() {
+//	frame := NewNewNameWithoutIndexing()
+//	frame.NameLength = 11
+//	frame.NameString = "mynewheader"
+//	frame.ValueLength = 5
+//	frame.ValueString = "first"
+//	buf := EncodeHeader(frame)
+//	log.Printf("%v", buf.Bytes())
+//}
 
 func main() {
-
 	var header = http.Header{
 		":scheme":     []string{"http"},
 		":path":       []string{"/index.html"},
 		"mynewheader": []string{"first"},
 	}
-
-	log.SetFlags(log.Lshortfile)
-
 	_ = header
 
-	headerTable := NewRequestHeaderTable()
-
-	log.Println(headerTable.SearchHeader(":scheme", "http"))
-	log.Println(headerTable.SearchHeader(":path", "/index.html"))
+	TestIndexedHeader()
 }
