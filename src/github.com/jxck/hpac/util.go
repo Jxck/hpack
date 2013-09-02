6130f5a564636005b84da3b681d4fe60b027ef34
Jxck <block.rxckin.beats@gmail.com>
2013-09-02 21:39:45 +0900

    add util.go CompareSlice

---
+++ src/github.com/jxck/hpac/util.go
@@ -0,0 +1,14 @@
+package hpac
+
+func CompareSlice(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
