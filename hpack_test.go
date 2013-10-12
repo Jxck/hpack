976af2af7f3e1b1edf82178ecc0b3db5a65c012a
Jxck <block.rxckin.beats@gmail.com>
2013-10-12 16:23:10 +0900

    update test

--- hpack_test.go
+++ hpack_test.go
@@ -3,6 +3,7 @@ package hpack
 import (
 	"encoding/base64"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -14,31 +15,25 @@ func init() {
 }
 
 func TestHpack(t *testing.T) {
-	jsoncase := `{
-   "wire": "Rkp0ZXh0L2h0bWwsYXBwbGljYXRpb24veGh0bWwreG1sLGFwcGxpY2F0aW9uL3htbDtxPTAuOSxpbWFnZS93ZWJwLCovKjtxPTAuOEgRZ3ppcCxkZWZsYXRlLHNkY2hJF2VuLVVTLGVuO3E9MC44LGphO3E9MC42UAltYXgtYWdlPTBRCmtlZXAtYWxpdmVABGhvc3QSYmxvZy5zdW1tZXJ3aW5kLmpwTHdNb3ppbGxhLzUuMCAoTWFjaW50b3NoOyBJbnRlbCBNYWMgT1MgWCAxMF84XzUpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8zMC4wLjE1OTkuNjkgU2FmYXJpLzUzNy4zNg==",
-   "header": {
-     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-     "accept-encoding": "gzip,deflate,sdch",
-     "accept-language": "en-US,en;q=0.8,ja;q=0.6",
-     "cache-control": "max-age=0",
-     "connection":  "keep-alive",
-     "host": "blog.summerwind.jp",
-     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36"
-   }
- }`
+	data, err := ioutil.ReadFile("./hpack-test-case/case_0.json")
+	if err != nil {
+		t.Fatal()
+	}
+	jsoncase := string(data)
 
 	type TestCase struct {
 		Wire   string
 		Header map[string]string
 	}
 
-	var testcase TestCase
+	var testcases []TestCase
 	dec := json.NewDecoder(strings.NewReader(jsoncase))
-	err := dec.Decode(&testcase)
+	err = dec.Decode(&testcases)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	testcase := testcases[0]
 	header := http.Header{}
 	for k, v := range testcase.Header {
 		header.Add(k, v)
