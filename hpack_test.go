3290e5c39ad14bfd959c8520d4cd4f98530e4c01
Jxck <block.rxckin.beats@gmail.com>
2013-10-16 17:16:40 +0900

    fix test

--- hpack_test.go
+++ hpack_test.go
@@ -15,28 +15,6 @@ func init() {
 
 const TestCaseDir string = "./hpack-test-case"
 
-func TestStory(t *testing.T) {
-	jsoncase := `
-[
-  {
-    "context": "request",
-    "wire": "Rkp0ZXh0L2h0bWwsYXBwbGljYXRpb24veGh0bWwreG1sLGFwcGxpY2F0aW9uL3htbDtxPTAuOSxpbWFnZS93ZWJwLCovKjtxPTAuOEgRZ3ppcCxkZWZsYXRlLHNkY2hJF2VuLVVTLGVuO3E9MC44LGphO3E9MC42UAltYXgtYWdlPTBRCmtlZXAtYWxpdmVABGhvc3QSYmxvZy5zdW1tZXJ3aW5kLmpwTHdNb3ppbGxhLzUuMCAoTWFjaW50b3NoOyBJbnRlbCBNYWMgT1MgWCAxMF84XzUpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8zMC4wLjE1OTkuNjkgU2FmYXJpLzUzNy4zNg==",
-    "header": {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-      "accept-encoding": "gzip,deflate,sdch",
-      "accept-language": "en-US,en;q=0.8,ja;q=0.6",
-      "cache-control": "max-age=0",
-      "connection": "keep-alive",
-      ":host": "blog.summerwind.jp",
-      "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36"
-    }
-  }
-]
-	`
-	testcases := toJSON(jsoncase)
-	RunStory(testcases, t)
-}
-
 type TestCase struct {
 	Context string
 	Wire    string
@@ -87,15 +65,46 @@ func RunStory(testcases []TestCase, t *testing.T) {
 	}
 }
 
+func TestStory(t *testing.T) {
+	jsoncase := `
+[
+  {
+    "context": "request",
+    "wire": "Rkp0ZXh0L2h0bWwsYXBwbGljYXRpb24veGh0bWwreG1sLGFwcGxpY2F0aW9uL3htbDtxPTAuOSxpbWFnZS93ZWJwLCovKjtxPTAuOEgRZ3ppcCxkZWZsYXRlLHNkY2hJF2VuLVVTLGVuO3E9MC44LGphO3E9MC42UAltYXgtYWdlPTBRCmtlZXAtYWxpdmVABGhvc3QSYmxvZy5zdW1tZXJ3aW5kLmpwTHdNb3ppbGxhLzUuMCAoTWFjaW50b3NoOyBJbnRlbCBNYWMgT1MgWCAxMF84XzUpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8zMC4wLjE1OTkuNjkgU2FmYXJpLzUzNy4zNg==",
+    "header": {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "accept-encoding": "gzip,deflate,sdch",
+      "accept-language": "en-US,en;q=0.8,ja;q=0.6",
+      "cache-control": "max-age=0",
+      "connection": "keep-alive",
+      ":host": "blog.summerwind.jp",
+      "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36"
+    }
+  }
+]
+	`
+	testcases := toJSON(jsoncase)
+	for _, testcase := range testcases {
+		RunStory([]TestCase{testcase}, t)
+	}
+}
+
 func TestHpack(t *testing.T) {
-	t.Skip() // TODO:fix me
 	files, err := ioutil.ReadDir(TestCaseDir)
 	if err != nil {
 		t.Fatal()
 	}
 	for _, file := range files {
 		if strings.HasPrefix(file.Name(), "case") {
-			// RunCase(file.Name(), t)
+			data, err := ioutil.ReadFile(TestCaseDir + "/" + file.Name())
+			if err != nil {
+				t.Fatal()
+			}
+			testcases := toJSON(string(data))
+			for _, testcase := range testcases {
+				log.Println("run file", file.Name())
+				RunStory([]TestCase{testcase}, t)
+			}
 		}
 		if strings.HasPrefix(file.Name(), "story") {
 			// RunStory(file.Name(), t)
