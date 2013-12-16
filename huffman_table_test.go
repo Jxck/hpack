8dd44b100c5b9d5e69479a8234afcf25d7d2808b
Jxck <block.rxckin.beats@gmail.com>
2013-12-16 23:19:27 +0900

    add huffencode test

--- huffman_table_test.go
+++ huffman_table_test.go
@@ -5,19 +5,30 @@ import (
 	"testing"
 )
 
-func TestHuffmanEncode(t *testing.T) {
-	expected := "db6d 883e 68d1 cb12 25ba 7f"
-	raw := []byte("www.example.com")
-	encoded := HuffmanEncode(raw)
-
-	actual := ""
-	for i, v := range encoded {
-		actual += fmt.Sprintf("%x", v)
-		if i%2 == 1 {
-			actual += fmt.Sprint(" ")
-		}
+func toHexString(hex []byte) (hexstr string) {
+	for _, v := range hex {
+		hexstr += fmt.Sprintf("%x", v)
 	}
-	if actual != expected {
-		t.Errorf("got %v\nwant %v", actual, expected)
+	return hexstr
+}
+
+var testcase = []struct {
+	str, hex string
+}{
+	{"www.example.com", "db6d883e68d1cb1225ba7f"},
+	{"no-cache", "63654a1398ff"},
+	{"custom-key", "4eb08b749790fa7f"},
+	{"custom-value", "4eb08b74979a17a8ff"},
+}
+
+func TestHuffmanEncode(t *testing.T) {
+	for _, tc := range testcase {
+		raw := []byte(tc.str)
+		expected := tc.hex
+		encoded := HuffmanEncode(raw)
+		actual := toHexString(encoded)
+		if actual != expected {
+			t.Errorf("\ngot  %v\nwant %v", actual, expected)
+		}
 	}
 }
