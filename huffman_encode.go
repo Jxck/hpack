dbc0ac8c37b38d8b3c6d26156a3aa958d5e1f118
Jxck <block.rxckin.beats@gmail.com>
2013-12-19 00:06:24 +0900

    type Byte to byt

--- huffman_encode.go
+++ huffman_encode.go
@@ -8,14 +8,14 @@ func init() {
 	log.SetFlags(log.Lshortfile)
 }
 
-// 結果 1 Byte を生成するための struct
-type Byte struct {
+// 結果 1 byte を生成するための struct
+type byt struct {
 	value  uint32
 	remain byte
 }
 
-func NewByte() *Byte {
-	return &Byte{0, 8}
+func newByt() *byt {
+	return &byt{0, 8}
 }
 
 func HuffmanEncodeRequest(raw []byte) (encoded []byte) {
@@ -32,25 +32,25 @@ func HuffmanEncode(raw []byte, table *HuffmanTable) (encoded []byte) {
 	}
 
 	// 1 byte の入れ物
-	byt := NewByte()
+	b := newByt()
 
-	for _, b := range raw {
+	for _, v := range raw {
 
 		// huffman table で変換
-		huff := table[b]
+		huff := table[v]
 
 		for huff.length > 0 { // huff.code を使いきるまで
 
-			if byt.remain > huff.length {
+			if b.remain > huff.length {
 				// huff.code の全てを入れる
 
 				// 左シフトして桁を合わせる
-				shift := byt.remain - huff.length
+				shift := b.remain - huff.length
 				tmp := huff.code << shift
 
 				// byt に追加、入れ多分だけ長さを減らす
-				byt.value += tmp
-				byt.remain = shift
+				b.value += tmp
+				b.remain = shift
 
 				// huff は空に
 				huff.length = 0
@@ -59,40 +59,40 @@ func HuffmanEncode(raw []byte, table *HuffmanTable) (encoded []byte) {
 				// huff.code の一部を入れる
 
 				// 右シフトして入れる分だけ切り出す
-				shift := huff.length - byt.remain
+				shift := huff.length - b.remain
 				tmp := huff.code >> shift
 
 				// byt に追加、もう入らない
-				byt.value += tmp
-				byt.remain = 0
+				b.value += tmp
+				b.remain = 0
 
 				// huff から使った分だけ減らす
 				huff.code -= (tmp << shift)
 				huff.length = shift
 			}
 
-			if byt.remain == 0 {
+			if b.remain == 0 {
 				// byt が埋まったら配列に移して初期化
-				encoded = append(encoded, byte(byt.value))
-				byt = NewByte()
+				encoded = append(encoded, byte(b.value))
+				b = newByt()
 			}
 		}
 	}
 
-	if byt.remain > 0 { // EOS でパディング
+	if b.remain > 0 { // EOS でパディング
 		// パディング分切り出す
 		eos := RequestHuffmanTable[256]
-		shift := eos.length - byt.remain
+		shift := eos.length - b.remain
 		padding := eos.code >> shift
 
 		// byt に加える
-		byt.value += padding
-		byt.remain = 0
+		b.value += padding
+		b.remain = 0
 
 		// 配列に移す
-		encoded = append(encoded, byte(byt.value))
+		encoded = append(encoded, byte(b.value))
 		// 最後なのでゼロ値でGC
-		byt = &Byte{}
+		b = &byt{}
 	}
 
 	return encoded
