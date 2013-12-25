890dc0e1a9a317e7e129223a5d9a45e1f4d42e67
Jxck <block.rxckin.beats@gmail.com>
2013-12-25 18:10:33 +0900

    update context test

--- context_test.go
+++ context_test.go
@@ -14,21 +14,41 @@ func TestRequestWithoutHuffman(t *testing.T) {
 		0x6d, 0x70, 0x6c, 0x65,
 		0x2e, 0x63, 0x6f, 0x6d,
 	}
-	client := NewContext()
-	client.Decode(buf)
 
-	expected := http.Header{
+	expectedHeader := http.Header{
 		"Method":    []string{"GET"},
 		"Scheme":    []string{"http"},
 		"Path":      []string{"/"},
 		"Authority": []string{"www.example.com"},
 	}
 
+	expectedHeaderFields := []HeaderField{
+		HeaderField{":authority", "www.example.com"},
+		HeaderField{":path", "/"},
+		HeaderField{":scheme", "http"},
+		HeaderField{":method", "GET"},
+	}
+
+	client := NewContext()
+	client.Decode(buf)
+
+	// test Header Table
 	if client.HT.Size() != 180 {
 		t.Errorf("got %v\nwant %v", client.HT.Size(), 180)
 	}
 
-	if !reflect.DeepEqual(client.ES.Header, expected) {
-		t.Errorf("got %v\nwant %v", client.ES.Header, expected)
+	// test Header Table
+	for i, hf := range expectedHeaderFields {
+		t.Log(*client.HT.HeaderFields[i] == hf)
+	}
+
+	// test Emitted Set
+	if !reflect.DeepEqual(client.ES.Header, expectedHeader) {
+		t.Errorf("got %v\nwant %v", client.ES.Header, expectedHeader)
+	}
+
+	// test Reference Set
+	for i, hf := range *client.RS {
+		t.Log(i, hf)
 	}
 }
