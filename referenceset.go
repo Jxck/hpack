d85ce44819ce076ebb70716ba6d98a95d5f0ede8
Jxck <block.rxckin.beats@gmail.com>
2013-12-25 15:00:52 +0900

    add ReferenceSet Add(), Len(), Empty()

--- referenceset.go
+++ referenceset.go
@@ -2,3 +2,19 @@ package hpack
 
 // an unordered set of references to entries of the header table.
 type ReferenceSet []int
+
+func NewReferenceSet() *ReferenceSet {
+	return &ReferenceSet{}
+}
+
+func (rs *ReferenceSet) Len() int {
+	return len(*rs)
+}
+
+func (rs *ReferenceSet) Add(index int) {
+	*rs = append(*rs, index)
+}
+
+func (rs *ReferenceSet) Empty() {
+	*rs = ReferenceSet{}
+}
--- referenceset_test.go
+++ referenceset_test.go
@@ -1,5 +1,22 @@
 package hpack
 
 import (
-//	"testing"
+	"testing"
 )
+
+func TestReferenceSetAdd(t *testing.T) {
+	rs := NewReferenceSet()
+	rs.Add(1)
+	if rs.Len() != 1 {
+		t.Errorf("\ngot  %v\nwant %v", rs.Len(), 1)
+	}
+}
+
+func TestReferenceSetEmpty(t *testing.T) {
+	rs := NewReferenceSet()
+	rs.Add(1)
+	rs.Empty()
+	if rs.Len() != 0 {
+		t.Errorf("\ngot  %v\nwant %v", rs.Len(), 0)
+	}
+}
