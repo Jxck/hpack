2eec5907665e7b5fa6410267d719e2e2f83c2570
Jxck <block.rxckin.beats@gmail.com>
2014-05-06 13:31:40 +0900

    refuctoring test

--- decoder_test.go
+++ decoder_test.go
@@ -6,81 +6,66 @@ import (
 	"testing"
 )
 
-func TestIndexedHeaderDecode(t *testing.T) {
-	buf := swrap.New([]byte{0x82})
-
-	// expected
-	var index uint64 = 2
-
-	decoded := DecodeHeader(&buf)
-	frame, ok := decoded.(*IndexedHeader)
-	if !ok {
-		t.Errorf("Decoded to incorrect frame type: %T", frame)
+func TestStringLiteralDecode(t *testing.T) {
+	// D.1.1.  Literal Header Field with Indexing
+	var indexing bool = true
+	var name, value string = "custom-key", "custom-header"
+	buf := []byte{
+		0x00, 0x0a,
+		0x63, 0x75,
+		0x73, 0x74,
+		0x6f, 0x6d,
+		0x2d, 0x6b,
+		0x65, 0x79,
+		0x0d, 0x63,
+		0x75, 0x73,
+		0x74, 0x6f,
+		0x6d, 0x2d,
+		0x68, 0x65,
+		0x61, 0x64,
+		0x65, 0x72,
 	}
-	assert.Equal(t, frame.Index, index)
+
+	sw := swrap.New(buf)
+	decoded := DecodeHeader(&sw)
+	frame, ok := decoded.(*StringLiteral)
+	assert.Equal(t, ok, true)
+	expected := NewStringLiteral(indexing, name, value)
+
+	assert.Equal(t, frame, expected)
 }
 
-func TestIndexedLiteralDecode_NoIndexing_NoHuffman(t *testing.T) {
-	buf := swrap.New([]byte{
-		0x44, 0x0c, 0x2f, 0x73,
-		0x61, 0x6d, 0x70, 0x6c,
-		0x65, 0x2f, 0x70, 0x61,
-		0x74, 0x68,
-	})
-
-	// expected
+func TestIndexedLiteralDecode(t *testing.T) {
+	// D.1.2.  Literal Header Field without Indexing
 	var indexing bool = false
 	var index uint64 = 4
 	var value string = "/sample/path"
-
-	decoded := DecodeHeader(&buf)
-	frame, ok := decoded.(*IndexedLiteral)
-	if !ok {
-		t.Errorf("Decoded to incorrect frame type: %T", frame)
+	buf := []byte{
+		0x44, 0x0c,
+		0x2f, 0x73,
+		0x61, 0x6d,
+		0x70, 0x6c,
+		0x65, 0x2f,
+		0x70, 0x61,
+		0x74, 0x68,
 	}
-	assert.Equal(t, frame, NewIndexedLiteral(indexing, index, value))
+
+	sw := swrap.New(buf)
+	decoded := DecodeHeader(&sw)
+	frame, ok := decoded.(*IndexedLiteral)
+	assert.Equal(t, ok, true)
+	expected := NewIndexedLiteral(indexing, index, value)
+	assert.Equal(t, frame, expected)
 }
 
-func TestStringLiteralDecode_Indexing_NoHuffman(t *testing.T) {
-	buf := swrap.New([]byte{
-		0x00, 0x0a, 0x63, 0x75,
-		0x73, 0x74, 0x6f, 0x6d,
-		0x2d, 0x6b, 0x65, 0x79,
-		0x0d, 0x63, 0x75, 0x73,
-		0x74, 0x6f, 0x6d, 0x2d,
-		0x68, 0x65, 0x61, 0x64,
-		0x65, 0x72,
-	})
+func TestIndexedHeaderDecode(t *testing.T) {
+	// D.1.3.  Indexed Header Field
+	var index uint64 = 2
+	buf := []byte{0x82}
 
-	// expected
-	var indexing bool = true
-	var name, value string = "custom-key", "custom-header"
-
-	decoded := DecodeHeader(&buf)
-	frame, ok := decoded.(*StringLiteral)
-	if !ok {
-		t.Errorf("Decoded to incorrect frame type: %T", frame)
-	}
-	assert.Equal(t, frame, NewStringLiteral(indexing, name, value))
-}
-
-func TestIndexedLiteralDecode_Indexing_Huffman(t *testing.T) {
-	buf := swrap.New([]byte{
-		0x04, 0x8b, 0xdb, 0x6d,
-		0x88, 0x3e, 0x68, 0xd1,
-		0xcb, 0x12, 0x25, 0xba,
-		0x7f,
-	})
-
-	// expected
-	var indexing bool = true
-	var index uint64 = 4
-	var value string = "www.example.com"
-
-	decoded := DecodeHeader(&buf)
-	frame, ok := decoded.(*IndexedLiteral)
-	if !ok {
-		t.Errorf("Decoded to incorrect frame type: %T", frame)
-	}
-	assert.Equal(t, frame, NewIndexedLiteral(indexing, index, value))
+	sw := swrap.New(buf)
+	decoded := DecodeHeader(&sw)
+	frame, ok := decoded.(*IndexedHeader)
+	assert.Equal(t, ok, true)
+	assert.Equal(t, frame.Index, index)
 }
--- encoder_test.go
+++ encoder_test.go
@@ -5,133 +5,58 @@ import (
 	"testing"
 )
 
-func TestIndexedHeaderEncode(t *testing.T) {
-	var index uint64
-	var frame *IndexedHeader
+func TestStringLiteralEncode(t *testing.T) {
+	// D.1.1.  Literal Header Field with Indexing
+	var indexing bool = true
+	var name, value string = "custom-key", "custom-header"
+	buf := []byte{
+		0x00, 0x0a,
+		0x63, 0x75,
+		0x73, 0x74,
+		0x6f, 0x6d,
+		0x2d, 0x6b,
+		0x65, 0x79,
+		0x0d, 0x63,
+		0x75, 0x73,
+		0x74, 0x6f,
+		0x6d, 0x2d,
+		0x68, 0x65,
+		0x61, 0x64,
+		0x65, 0x72,
+	}
 
-	index = 2
-	frame = NewIndexedHeader(index)
+	frame := NewStringLiteral(indexing, name, value)
 	actual := frame.Encode()
-	expected := []byte{0x82}
-	assert.Equal(t, actual.Bytes(), expected)
 
-	index = 180
-	frame = NewIndexedHeader(index)
-	actual = frame.Encode()
-	expected = []byte{0xFF, 0x35}
-	assert.Equal(t, actual.Bytes(), expected)
+	assert.Equal(t, actual.Bytes(), buf)
 }
 
 func TestIndexedLiteralEncode(t *testing.T) {
-	var indexing bool
-	var index uint64
-	var value string
-
-	// No Indexing
-	indexing = false
-	index = 4
-	value = "/sample/path"
-	expected := []byte{
-		0x44, 0x0c, 0x2f, 0x73,
-		0x61, 0x6d, 0x70, 0x6c,
-		0x65, 0x2f, 0x70, 0x61,
+	// D.1.2.  Literal Header Field without Indexing
+	var indexing bool = false
+	var index uint64 = 4
+	var value string = "/sample/path"
+	buf := []byte{
+		0x44, 0x0c,
+		0x2f, 0x73,
+		0x61, 0x6d,
+		0x70, 0x6c,
+		0x65, 0x2f,
+		0x70, 0x61,
 		0x74, 0x68,
 	}
 
 	frame := NewIndexedLiteral(indexing, index, value)
 	actual := frame.Encode()
-
-	assert.Equal(t, actual.Bytes(), expected)
-
-	// Indexing
-	indexing = true
-	index = 180
-	value = "/sample/path"
-	expected = []byte{
-		0x3f, 0x75, 0x0c, 0x2f,
-		0x73, 0x61, 0x6d, 0x70,
-		0x6c, 0x65, 0x2f, 0x70,
-		0x61, 0x74, 0x68,
-	}
-
-	frame = NewIndexedLiteral(indexing, index, value)
-	actual = frame.Encode()
-	assert.Equal(t, actual.Bytes(), expected)
+	assert.Equal(t, actual.Bytes(), buf)
 }
 
-func TestIndexedLiteralEncodeHuffman(t *testing.T) {
-	var indexing bool
-	var index uint64
-	var value string
+func TestIndexedHeaderEncode(t *testing.T) {
+	// D.1.3.  Indexed Header Field
+	var index uint64 = 2
+	buf := []byte{0x82}
 
-	// Indexing
-	indexing = true
-	index = 4
-	value = "www.example.com"
-	expected := []byte{
-		0x04, 0x8b, 0xdb, 0x6d,
-		0x88, 0x3e, 0x68, 0xd1,
-		0xcb, 0x12, 0x25, 0xba,
-		0x7f,
-	}
-
-	frame := NewIndexedLiteral(indexing, index, value)
-	actual := frame.EncodeHuffman()
-	assert.Equal(t, actual.Bytes(), expected)
-}
-
-func TestStringLiteralEncode(t *testing.T) {
-	var indexing bool
-	var name, value string = "custom-key", "custom-header"
-
-	// No Indexing
-	indexing = false
-	expected := []byte{
-		0x40, 0x0a, 0x63, 0x75,
-		0x73, 0x74, 0x6f, 0x6d,
-		0x2d, 0x6b, 0x65, 0x79,
-		0x0d, 0x63, 0x75, 0x73,
-		0x74, 0x6f, 0x6d, 0x2d,
-		0x68, 0x65, 0x61, 0x64,
-		0x65, 0x72,
-	}
-
-	frame := NewStringLiteral(indexing, name, value)
+	frame := NewIndexedHeader(index)
 	actual := frame.Encode()
-	assert.Equal(t, actual.Bytes(), expected)
-
-	// Indexing
-	indexing = true
-	expected = []byte{
-		0x00, 0x0a, 0x63, 0x75,
-		0x73, 0x74, 0x6f, 0x6d,
-		0x2d, 0x6b, 0x65, 0x79,
-		0x0d, 0x63, 0x75, 0x73,
-		0x74, 0x6f, 0x6d, 0x2d,
-		0x68, 0x65, 0x61, 0x64,
-		0x65, 0x72,
-	}
-
-	frame = NewStringLiteral(indexing, name, value)
-	actual = frame.Encode()
-	assert.Equal(t, actual.Bytes(), expected)
-}
-
-func TestStringLiteralEncodeHuffman(t *testing.T) {
-	var indexing bool
-	var name, value string = "custom-key", "custom-value"
-
-	// Indexing
-	indexing = true
-	expected := []byte{
-		0x00, 0x88, 0x4e, 0xb0,
-		0x8b, 0x74, 0x97, 0x90,
-		0xfa, 0x7f, 0x89, 0x4e,
-		0xb0, 0x8b, 0x74, 0x97,
-		0x9a, 0x17, 0xa8, 0xff,
-	}
-
-	frame := NewStringLiteral(indexing, name, value)
-	actual := frame.EncodeHuffman()
-	assert.Equal(t, actual.Bytes(), expected)
+	assert.Equal(t, actual.Bytes(), buf)
 }
