5c0892a7270ba0d80599041115581a860ccccb82
Jxck <block.rxckin.beats@gmail.com>
2014-05-06 13:46:31 +0900

    up encoder.go

--- encoder.go
+++ encoder.go
@@ -16,20 +16,34 @@ func (frame *IndexedHeader) Encode() (buf *swrap.SWrap) {
 }
 
 func (frame *IndexedLiteral) Encode() (buf *swrap.SWrap) {
-	buf = swrap.Make(integer.Encode(frame.Index, 6))
-	if !frame.Indexing {
-		(*buf)[0] += 0x40
+	switch frame.Indexing {
+	case WITH:
+		buf = swrap.Make(integer.Encode(frame.Index, 6))
+		(*buf)[0] += 0x20
+	case WITHOUT:
+		buf = swrap.Make(integer.Encode(frame.Index, 4))
+	case NEVER:
+		buf = swrap.Make(integer.Encode(frame.Index, 4))
+		(*buf)[0] += 0x8
 	}
+
 	// No Huffman
 	buf.Merge(integer.Encode(frame.ValueLength, 7))
 	buf.Merge([]byte(frame.ValueString))
+
 	return buf
 }
 
 func (frame *IndexedLiteral) EncodeHuffman() (buf *swrap.SWrap) {
-	buf = swrap.Make(integer.Encode(frame.Index, 6))
-	if !frame.Indexing {
-		(*buf)[0] += 0x40
+	switch frame.Indexing {
+	case WITH:
+		buf = swrap.Make(integer.Encode(frame.Index, 6))
+		(*buf)[0] += 0x20
+	case WITHOUT:
+		buf = swrap.Make(integer.Encode(frame.Index, 4))
+	case NEVER:
+		buf = swrap.Make(integer.Encode(frame.Index, 4))
+		(*buf)[0] += 0x8
 	}
 
 	var encoded, length []byte
@@ -46,33 +60,41 @@ func (frame *IndexedLiteral) EncodeHuffman() (buf *swrap.SWrap) {
 
 func (frame *StringLiteral) Encode() (buf *swrap.SWrap) {
 	buf = new(swrap.SWrap)
-	if frame.Indexing {
-		buf.Add(0) // 0000 0000
-	} else {
+	switch frame.Indexing {
+	case WITH:
 		buf.Add(0x40) // 0100 0000
+	case WITHOUT:
+		buf.Add(0) // 0000 0000
+	case NEVER:
+		buf.Add(0x10) // 0001 0000
 	}
+
 	// No Huffman
 	buf.Merge(integer.Encode(frame.NameLength, 7))
 	buf.Merge([]byte(frame.NameString))
+
 	// No Huffman
 	buf.Merge(integer.Encode(frame.ValueLength, 7))
 	buf.Merge([]byte(frame.ValueString))
+
 	return buf
 }
 
 func (frame *StringLiteral) EncodeHuffman() (buf *swrap.SWrap) {
 	buf = new(swrap.SWrap)
-	if frame.Indexing {
-		buf.Add(0) // 0000 0000
-	} else {
+	switch frame.Indexing {
+	case WITH:
 		buf.Add(0x40) // 0100 0000
+	case WITHOUT:
+		buf.Add(0) // 0000 0000
+	case NEVER:
+		buf.Add(0x10) // 0001 0000
 	}
 
 	var encoded, length []byte
 
 	// Name With Huffman
 	encoded = huffman.Encode([]byte(frame.NameString))
-
 	length = integer.Encode(uint64(len(encoded)), 7)
 	length[0] += 0x80 // 1000 0000 (huffman flag)
 	buf.Merge(length)
