5daf1f4086cbe2e18a03e275b4c016c2b1a87214
Jxck <block.rxckin.beats@gmail.com>
2014-05-06 10:30:54 +0900

    use assertion in header_field_test

--- header_field_test.go
+++ header_field_test.go
@@ -1,12 +1,11 @@
 package hpack
 
 import (
+	assert "github.com/jxck/assertion"
 	"testing"
 )
 
 func TestHeaderFieldSize(t *testing.T) {
 	h := NewHeaderField("hello", "world")
-	if h.Size() != 42 {
-		t.Errorf("got %v\nwant %v", h.Size(), 42)
-	}
+	assert.Equal(t, h.Size(), 42)
 }
