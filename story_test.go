0baf987fa2fe4aac0fd09b4b2dc05714c58514df
Jxck <block.rxckin.beats@gmail.com>
2014-09-03 00:07:35 +0900

    pass normal story deocde test

--- story_test.go
+++ story_test.go
@@ -90,20 +90,17 @@ func RunStory(testfile TestFile, t *testing.T) {
 
 func TestStory(t *testing.T) {
 	dirs := []string{
-	// "./hpack-test-case/go-hpack/",
-	// "./hpack-test-case/haskell-http2-diff/",
-	// "./hpack-test-case/haskell-http2-diff-huffman/",
-	// "./hpack-test-case/haskell-http2-linear/",
-	// "./hpack-test-case/haskell-http2-linear-huffman/",
-	// "./hpack-test-case/haskell-http2-naive/",
-	// "./hpack-test-case/haskell-http2-naive-huffman/",
-	// "./hpack-test-case/haskell-http2-static/",
-	// "./hpack-test-case/haskell-http2-static-huffman/",
-	// "./hpack-test-case/nghttp2/",
-	// "./hpack-test-case/nghttp2-16384-4096/",
-	// "./hpack-test-case/node-http2-hpack/",
-	// "./hpack-test-case/node-http2-protocol/",
-	// "./hpack-test-case/twitter-hpack/",
+		// TODO: "./hpack-test-case/go-hpack/",
+		"./hpack-test-case/haskell-http2-linear-huffman/",
+		"./hpack-test-case/haskell-http2-linear/",
+		"./hpack-test-case/haskell-http2-naive-huffman/",
+		"./hpack-test-case/haskell-http2-naive/",
+		"./hpack-test-case/haskell-http2-static-huffman/",
+		"./hpack-test-case/haskell-http2-static/",
+		// TODO: "./hpack-test-case/nghttp2-16384-4096/",
+		// TODO: "./hpack-test-case/nghttp2-change-table-size/",
+		"./hpack-test-case/nghttp2/",
+		"./hpack-test-case/node-http2-hpack/",
 	}
 
 	for _, dir := range dirs {
